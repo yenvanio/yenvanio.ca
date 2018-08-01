26abe6df09103b8575a799e75d385c0d9a638020
Ubuntu <ubuntu@ip-172-31-21-71.ec2.internal>
2018-08-01 23:20:06 +0000

    Re - Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# yshiv.com
+Personal Website
---
+++ js/customJs.js
@@ -0,0 +1,11 @@
+jQuery(document).ready(function(){
+	jQuery('.skillbar').each(function(){
+		jQuery(this).find('.skillbar-bar').animate({
+			width:jQuery(this).attr('data-percent')
+		},6000);
+	});
+});
+
+$(document).on('click', '[id$="blogLink"]', function() {
+        alert("Coming Soon!");
+    });
